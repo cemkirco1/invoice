f15b98074b3d590175b996eeee7a01bcad33200b
Cem Kırkoyun <cemkirco1@gmail.com>
2023-07-20 07:20:59 +0300

    add paid component to bills screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import Header from "../src/components/Header";
+import Paid from "../src/components/Bills";
+
 const IndexPage = () => (
   <div>
     <Header />
+    <Paid />
   </div>
 );
 
