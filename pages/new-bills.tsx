71af9e5615bab7dd2e09b1938a0c8399dd680232
Cem Kırkoyun <cemkirco1@gmail.com>
2023-07-20 07:22:11 +0300

    Unused imports have been deleted.

--- pages/new-bills.tsx
+++ pages/new-bills.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
 import Header from "../src/components/Header";
-import Paid from "../src/components/Bills";
+
 import NewInvoice from "../src/components/NewInvoice";
 
 const IndexPage = () => (
